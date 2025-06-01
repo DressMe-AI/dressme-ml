4b63a4f83038e6d7e9a855ffaf3e2697233de226
yaniker <90459089+yaniker@users.noreply.github.com>
2025-06-01 20:47:59 +0000

    added dependencies

--- sagemaker_launcher.py
+++ sagemaker_launcher.py
@@ -30,6 +30,7 @@ estimator = TensorFlow(
     },
     output_path=f"s3://{sagemaker_session.default_bucket()}/dressmeai/output",
     base_job_name="dressmeai-train",
+    dependencies=["src/requirements.txt"],
 )
 
 # Launch training
